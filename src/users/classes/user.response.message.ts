5842f73875a4b38005464b478065e94562309e20
haeseung123 <showui96@gmail.com>
2023-11-02 00:07:23 +0900

    chore: 응답 메시지 추가
    - 위치 업데이트, 점심 추천 업데이트, 사용자 정보 조회 메시지
    
    feat-#28

--- src/users/classes/user.response.message.ts
+++ src/users/classes/user.response.message.ts
@@ -3,4 +3,7 @@ export class UserResponseMesaage {
 	static SIGN_IN = '로그인 성공';
 	static LOG_OUT = '로그아웃 성공';
 	static REFRESH = '토큰 새로고침 성공';
+	static LOCATION = '유저 위치 업데이트 성공';
+	static LUNCH_RECOMM = '점심 추천 업데이트 성공';
+	static GET_USER = '사용자 조회 성공';
 }
