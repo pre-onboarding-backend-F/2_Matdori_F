1f802fdd4e1efb80b67421948c2551529ceb37bf
cabbage556 <liberty556@gmail.com>
2023-11-01 12:27:38 +0900

    feat: 맛집 목록 조회 쿼리 파라미터 enum 추가
    - range, orderBy, order 쿼리 파라미터에 사용할 enum을 추가하였음
    - RestaurantQueryRange enum: range 쿼리 파라미터에 적용(1.0, 5.0, 10.0)
    - RestaurantQueryOrderBy enum: orderBy 쿼리 파라미터에 적용(dist, rating)
    - RestaurantQueryOrder enum: order 쿼리 파라미터에 적용(desc, asc)
    
    feat-#12

---
+++ src/restaurants/enums/restaurant-query.enum.ts
@@ -0,0 +1,15 @@
+export enum RestaurantQueryRange {
+	ONE_KM = '1.0',
+	FIVE_KM = '5.0',
+	TEN_KM = '10.0',
+}
+
+export enum RestaurantQueryOrderBy {
+	DIST = 'dist',
+	RATING = 'rating',
+}
+
+export enum RestaurantQueryOrder {
+	DESC = 'desc',
+	ASC = 'asc',
+}
