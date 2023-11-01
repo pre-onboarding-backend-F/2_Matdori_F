41fbcb570c5584c7b94138e830eb59345641b756
cabbage556 <liberty556@gmail.com>
2023-11-01 10:48:40 +0900

    feat: 맛집 상세 조회 API
    기능 구현
    - id 패스 파라미터에 CustomParseUUIDPipe 적용
    - id에 해당하는 맛집 레코드 조회 후 존재하지 않으면 NotFoundException 예외 반환
    - id에 해당하는 맛집 레코드가 존재하면 해당 레코드를 리턴
    - 컨트롤러 범위에 HttpExceptionFilter 적용
    
    추가 필요한 부분
    - 응답 성공 시 message 반환하기(ResponseMessage 데코레이터, 인터셉터 적용)
    - 인증 가드(구현되면 적용할 예정)
    
    feat-#8

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseFilters } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
+import { Restaurant } from './entity/restaurant.entity';
+import { HttpExceptionFilter } from 'src/global/filters/http-exception.filter';
+import { CustomParseUUIDPipe } from './pipes/custom-parse-uuid.pipe';
 
 @Controller('restaurants')
+@UseFilters(HttpExceptionFilter)
 export class RestaurantsController {
 	constructor(private readonly restaurantsService: RestaurantsService) {}
+
+	@Get(':id')
+	findOne(@Param('id', CustomParseUUIDPipe) id: string): Promise<Restaurant> {
+		return this.restaurantsService.findOne(id);
+	}
 }
