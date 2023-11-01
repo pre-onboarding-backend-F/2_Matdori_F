cf39fd4f3c6fa13133c8831b3460530041da4185
cabbage556 <liberty556@gmail.com>
2023-11-01 17:08:35 +0900

    feat: RestaurantsController 라우트 핸들러 ResponseMessage 데코레이터 적용
    - 각 라우트 핸들러에 해당하는 응답 성공 메세지를 전달하도록 ResponseMessage 데코레이터를 적용하였음
    
    feat-#22

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -4,6 +4,8 @@ import { Restaurant } from './entity/restaurant.entity';
 import { HttpExceptionFilter } from 'src/global/filters/http-exception.filter';
 import { CustomParseUUIDPipe } from './pipes/custom-parse-uuid.pipe';
 import { GetPostsDto } from './dto/get-posts.dto';
+import { ResponseMessage } from 'src/global/decorators/response-key.decorator';
+import { RestaurantResponse } from './enums/restaurant-response.enum';
 
 @Controller('restaurants')
 @UseFilters(HttpExceptionFilter)
@@ -11,11 +13,13 @@ export class RestaurantsController {
 	constructor(private readonly restaurantsService: RestaurantsService) {}
 
 	@Get()
+	@ResponseMessage(RestaurantResponse.GET_POSTS)
 	getPosts(@Query() getPostsDto: GetPostsDto) {
 		console.log(getPostsDto);
 	}
 
 	@Get(':id')
+	@ResponseMessage(RestaurantResponse.FIND_ONE)
 	findOne(@Param('id', CustomParseUUIDPipe) id: string): Promise<Restaurant> {
 		return this.restaurantsService.findOne(id);
 	}
