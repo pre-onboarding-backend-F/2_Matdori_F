75c0950d1bb0a7f7728a129bb00a770804c39517
cabbage556 <liberty556@gmail.com>
2023-11-02 12:16:46 +0900

    feat: RestaurantException enum 프로퍼티 추가
    - CANNOT_SEARCH_CATEGORY 프로퍼티: RestaurantCategory에 해당하지 않는 카테고리가 아닌 경우 BadRequestException 예외를 던질 때 사용하는 예외 메세지
    
    fix-#32

--- src/restaurants/enums/restaurant-exception.enum.ts
+++ src/restaurants/enums/restaurant-exception.enum.ts
@@ -2,4 +2,5 @@ export enum RestaurantException {
 	INVALID_ID = '잘못된 id 형식입니다.',
 	NOT_FOUND = '존재하지 않는 맛집입니다.',
 	INVALID_PAGE = '유효하지 않은 페이지입니다.',
+	CANNOT_SEARCH_CATEGORY = '검색할 수 없는 카테고리입니다.',
 }
