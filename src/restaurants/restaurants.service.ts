30577cc845ee0b0af1475fbc7b4dcb89633533d8
cabbage556 <liberty556@gmail.com>
2023-11-02 12:19:02 +0900

    fix: getPosts 서비스 로직 수정
    수정 사항
    - getPosts 내부의 로직들을 메서드로 나누었음
    - searchByName: name 칼럼에 대해 search 키워드로 LIKE 검색 수행
    - searchByCategory: category 칼럼에 대해 search 키워드로 일치하는 레코드 검색 수행
    - orderByRating: 정렬 기준이 평점인 경우 rating 칼럼에 대해 오름차순, 내림차순 정렬 수행
    - getRestaurantsWithinRange: range 범위 안에 있는 모든 맛집을 선택함
    - orderByDistance: 정렬 기준이 거리인 경우 맛집과의 거리를 기준으로 오름차순, 내림차순 정렬 수행
    - 페이지네이션: 배열 slice 메서드에 page, count 쿼리파라미터의 값을 사용해 페이지네이션 적용
    
    fix-#32

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,13 +1,15 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Restaurant } from './entity/restaurant.entity';
-import { FindOptionsWhere, Repository } from 'typeorm';
+import { FindOptionsWhere, Repository, SelectQueryBuilder } from 'typeorm';
 import { RestaurantException } from './enums/restaurant-exception.enum';
 import { GetPostsDto } from './dto/get-posts.dto';
-import { latLonToKm } from 'src/global/utils/latLonToKm';
+import { latLonToKm } from 'src/global/utils/lat-lon-to-km';
 import { RestaurantQueryOrder, RestaurantQueryOrderBy, RestaurantQuerySearchBy } from './enums/restaurant-query.enum';
 import { PageMetaDto } from './dto/page-meta.dto';
 import { PageDto } from './dto/page.dto';
+import { getEnumKeys } from 'src/global/utils/get-enum-keys';
+import { RestaurantCategory } from './enums/restaurant-category.enum';
 
 @Injectable()
 export class RestaurantsService {
@@ -18,24 +20,36 @@ export class RestaurantsService {
 		private readonly restaurantRepository: Repository<Restaurant>,
 	) {}
 
-	async getPosts(getPostsDto: GetPostsDto) {
-		const { lat, lon, range, orderBy, order, searchBy, search, page, count } = getPostsDto;
-		const qb = this.restaurantRepository.createQueryBuilder(this.restaurant);
+	searchByName(qb: SelectQueryBuilder<Restaurant>, search: string) {
+		qb.where(`name like '%' || :search || '%'`, { search });
+	}
 
-		// 검색 기준에 따라 검색
-		if (searchBy === RestaurantQuerySearchBy.NAME) {
-			qb.where(`to_tsvector(${this.restaurant}.name) @@ to_tsquery('${search}')`);
+	searchByCategory(qb: SelectQueryBuilder<Restaurant>, search: string) {
+		let canSearch = false;
+		for (const key of getEnumKeys(RestaurantCategory)) {
+			const category = RestaurantCategory[key];
+			if (category === search) {
+				canSearch = true;
+				break;
+			}
 		}
+		if (!canSearch) throw new BadRequestException(RestaurantException.CANNOT_SEARCH_CATEGORY);
 
-		// 정렬 기준에 따라 정렬
-		if (orderBy === RestaurantQueryOrderBy.RATING) {
-			qb.addOrderBy(`${this.restaurant}.${orderBy}`, order === RestaurantQueryOrder.DESC ? 'DESC' : 'ASC');
-		}
+		qb.where('category = :search', { search });
+	}
 
-		// range 안쪽에 있는 맛집 선택
-		let queriedRestaurants = await qb.getMany();
-		const distanceMap = new Map();
-		queriedRestaurants = queriedRestaurants.filter((restaurant) => {
+	orderByRating(qb: SelectQueryBuilder<Restaurant>, order: RestaurantQueryOrder) {
+		qb.addOrderBy(`rating`, order === RestaurantQueryOrder.DESC ? 'DESC' : 'ASC');
+	}
+
+	getRestaurantsWithinRange(
+		distanceMap: Map<string, number>,
+		restaurants: Restaurant[],
+		lon: number,
+		lat: number,
+		range: number,
+	): Restaurant[] {
+		return restaurants.filter((restaurant) => {
 			const distance = latLonToKm({ point1: [lon, lat], point2: [restaurant.lon, restaurant.lat] });
 			if (distance < range) {
 				distanceMap.set(restaurant.id, distance);
@@ -43,17 +57,38 @@ export class RestaurantsService {
 			}
 			return false;
 		});
+	}
+
+	orderByDistance(distanceMap: Map<string, number>, restaurants: Restaurant[], order: RestaurantQueryOrder) {
+		restaurants.sort((a, b) => {
+			if (order === RestaurantQueryOrder.ASC) return distanceMap.get(a.id) - distanceMap.get(b.id);
+			else return distanceMap.get(b.id) - distanceMap.get(a.id);
+		});
+	}
+
+	async getPosts(getPostsDto: GetPostsDto) {
+		const { lat, lon, range, orderBy, order, searchBy, search, page, count } = getPostsDto;
+		const qb = this.restaurantRepository.createQueryBuilder(this.restaurant);
+
+		// 검색 기준에 따라 검색
+		if (searchBy === RestaurantQuerySearchBy.NAME) this.searchByName(qb, search);
+		else if (searchBy === RestaurantQuerySearchBy.CATEGORY) this.searchByCategory(qb, search);
+
+		// 정렬 기준에 따라 정렬
+		if (orderBy === RestaurantQueryOrderBy.RATING) this.orderByRating(qb, order);
+
+		// range 안쪽에 있는 맛집 선택
+		const distanceMap = new Map<string, number>();
+		let restaurants = await qb.getMany();
+		restaurants = this.getRestaurantsWithinRange(distanceMap, restaurants, lon, lat, range);
 
 		// 거리 기준 정렬
-		if (orderBy === RestaurantQueryOrderBy.DIST) {
-			queriedRestaurants.sort((a, b) => {
-				if (order === RestaurantQueryOrder.ASC) return distanceMap.get(a.id) - distanceMap.get(b.id);
-				else return distanceMap.get(b.id) - distanceMap.get(a.id);
-			});
-		}
+		if (orderBy === RestaurantQueryOrderBy.DIST) this.orderByDistance(distanceMap, restaurants, order);
 
-		const pageMeta = new PageMetaDto({ totalCounts: queriedRestaurants.length, page, count });
-		if (pageMeta.lastPage >= pageMeta.page) return new PageDto<Restaurant>(queriedRestaurants, pageMeta);
+		// 페이지네이션
+		const pageMeta = new PageMetaDto({ totalCounts: restaurants.length, page, count });
+		restaurants = restaurants.slice((page - 1) * count, (page - 1) * count + count);
+		if (pageMeta.lastPage >= pageMeta.page) return new PageDto<Restaurant>(restaurants, pageMeta);
 		else throw new ConflictException(RestaurantException.INVALID_PAGE);
 	}
 
