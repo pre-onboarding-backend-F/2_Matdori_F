3c206f4d476bcffde3f42f58cf9c1084fad35b7b
cabbage556 <liberty556@gmail.com>
2023-11-06 01:39:33 +0900

    fix: CitysCacheInterceptor 로직 수정
    수정 사항
    - key가 없는 경우만 확인하지 않고 key와 ttl이 모두 없는 경우를 확인
    - key와 ttl이 모두 없는 경우 CITYS_GET_CITIES 키와 CITYS_GET_CITIES_TTL ttl을 적용해 캐싱
    
    feat-#51

--- src/citys/interceptors/citys-cache.interceptor.ts
+++ src/citys/interceptors/citys-cache.interceptor.ts
@@ -3,7 +3,7 @@ import { CallHandler, ExecutionContext, Inject, Injectable, NestInterceptor } fr
 import { Reflector } from '@nestjs/core';
 import { Observable, of, tap } from 'rxjs';
 import { CacheService } from 'src/cache/cache.service';
-import { CITYS_GET_CITIES } from '../constants/city-cache-key.constants';
+import { CITYS_GET_CITIES, CITYS_GET_CITIES_TTL } from '../constants/citys-cache.constants';
 
 @Injectable()
 export class CitysCacheInterceptor implements NestInterceptor {
@@ -16,8 +16,9 @@ export class CitysCacheInterceptor implements NestInterceptor {
 		const key = this.reflector.get(CACHE_KEY_METADATA, context.getHandler()); // citys:getCities
 		const ttl = this.reflector.get(CACHE_TTL_METADATA, context.getHandler()); // 0
 
-		// 키가 없으면 DB에서 조회해 리턴하면서 캐싱
-		if (!key) return next.handle().pipe(tap((res) => this.cacheService.set(CITYS_GET_CITIES, res, ttl)));
+		// key와 ttl이 없으면 DB에서 조회해 리턴하면서 캐싱
+		if (!key && !ttl)
+			return next.handle().pipe(tap((res) => this.cacheService.set(CITYS_GET_CITIES, res, CITYS_GET_CITIES_TTL)));
 
 		try {
 			// 캐시 결과 리턴
@@ -25,11 +26,7 @@ export class CitysCacheInterceptor implements NestInterceptor {
 			if (citys) return of(citys);
 
 			// 캐시 결과가 없으면 DB에서 조회해 리턴하면서 캐싱
-			return next.handle().pipe(
-				tap((res) => {
-					this.cacheService.set(key, res, ttl);
-				}),
-			);
+			return next.handle().pipe(tap((res) => this.cacheService.set(key, res, ttl)));
 		} catch {
 			// cacheService에서 에러가 발생하는 경우 DB에서 조회
 			return next.handle();
